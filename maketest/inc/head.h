e712f910f893426bc6baa8c3270828f8a25ce8f5
monster22yao <65671045+monster22yao@users.noreply.github.com>
2020-08-24 11:45:04 +0800

    Add files via upload

---
+++ maketest/inc/head.h
@@ -0,0 +1,12 @@
+#ifndef _HEAD_H_
+#define _HEAD_H_
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<pthread.h>
+int add(int,int);
+int sub(int,int);
+int mul(int,int);
+int div1(int,int);
+#endif
